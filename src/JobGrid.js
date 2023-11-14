7c132177334ebae218df3b30635fcddcf6707455
hrabbani <humza.rabbani@gmail.com>
2023-11-13 20:34:22 -0500

    added jobs layout (grid messed up)

--- src/JobGrid.js
+++ src/JobGrid.js
@@ -1,81 +1,96 @@
 import React from "react";
 import TeamJob from "./TeamJob";
+import alpinelogo from "./team_logos/alpinepng.png";
+import williamslogo from "./team_logos/Logo Williams F1.png";
+import haasLogo from "./team_logos/Haas F1 Logo.jpg";
+import romeoLogo from "./team_logos/Alfa Romeo F1 Logo.png";
+import mercedezLogo from "./team_logos/mercedez-logo.jpeg";
+import martinLogo from "./team_logos/amartin-logo.jpeg";
+import ferrariLogo from "./team_logos/ferrarilogo.png";
+import tauriLogo from "./team_logos/AlphaTauri F1 Logo.png";
+import mclarenLogo from "./team_logos/maclaren-logo.png";
+import redbullLogo from "./team_logos/Red Bull Racing Logo.jpg";
 
 function JobGrid() {
   const teamJobs = [
     {
-      logo: "team_logos/redbull.jpg",
+      logo: redbullLogo,
       url: "https://www.redbullracing.com/int-en/jobs",
       name: "Red Bull",
     },
     {
-      logo: "logo2.png",
+      logo: mercedezLogo,
       url: "https://www.mercedesamgf1.com/careers/vacancies",
       name: "Mercedes",
     },
     {
-      logo: "logo3.png",
+      logo: ferrariLogo,
       url: "https://www.ferrari.com/en-EN/corporate/career",
       name: "Ferrari",
     },
     {
-      logo: "logo4.png",
+      logo: mclarenLogo,
       url: "https://racingcareers.mclaren.com/",
       name: "McLaren",
     },
     {
-      logo: "logo5.png",
+      logo: martinLogo,
       url: "https://www.astonmartinf1.com/en-GB/careers",
       name: "Aston Martin",
     },
     {
-      logo: "Users/kaladanwuke/Downloads/f1 logos/alpinepng.pngg",
+      logo: alpinelogo,
       url: "https://www.alpinecars.com/en/jobs​",
       name: "Alpine",
     },
     {
-      logo: "logo7.png",
+      logo: williamslogo,
       url: "https://group.williamsf1.com/careers",
       name: "Williams",
     },
     {
-      logo: "logo8.png",
+      logo: tauriLogo,
       url: "https://scuderia.alphatauri.com/en/f1-jobs/",
       name: "AlphaTauri",
     },
     {
-      logo: "/Users/kaladanwuke/Downloads/f1 logos/Alfa Romeo F1 Logo.png",
+      logo: romeoLogo,
       url: "https://www.sauber-group.com/corporate/jobs/",
       name: "Alfa Romeo",
     },
     {
-      logo: "logo10.png",
+      logo: haasLogo,
       url: "https://www.haasf1team.com/careers",
       name: "Haas",
     },
   ];
 
-  const rows = [];
-  for (let i = 0; i < 5; i++) {
-    const cols = [];
-    for (let j = 0; j < 2; j++) {
-      const teamJob = teamJobs[i * 2 + j];
-      cols.push(
-        <TeamJob logo={teamJob.logo} url={teamJob.url} name={teamJob.name} />
-      );
-    }
-    rows.push(
-      <div key={i} className="row">
-        {cols}
+  return (
+    <div className="job-grid">
+      <div className="row">
+        {teamJobs.slice(0, 5).map((teamJob) => (
+          <div className="col-2">
+            <TeamJob
+              logo={teamJob.logo}
+              url={teamJob.url}
+              teamName={teamJob.name}
+            />
+          </div>
+        ))}
       </div>
-    );
-  }
-  <TeamJob
-    logo="team_logos/haas.jpeg"
-    url="https://www.redbullracing.com/int-en/jobs"
-    name="Red Bull"
-  ></TeamJob>;
-  return <div className="job-grid">{rows}</div>;
+      <div className="row">
+        {teamJobs.slice(5).map((teamJob) => (
+          <div className="col-2">
+            <TeamJob
+              logo={teamJob.logo}
+              url={teamJob.url}
+              teamName={teamJob.name}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default JobGrid;
